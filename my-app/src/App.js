59e04c5ba5920411aa6b7e9930a432844869c2ea
Puneet Srivastava <puneet.srivastava>
2024-04-17 03:46:48 +0530

    Hompage code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App"> 
+     <Header />
      <Navbar />
      <AllRoutes />
     </div>
